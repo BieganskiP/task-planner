5b09f4ad35163ba55bc2a8eb018f5640ff182b5a
BieganskiP <bieganski1996@gmail.com>
2023-02-21 18:23:50 +0100

    finished login screen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -7,10 +7,17 @@ export default function WelcomeScreen() {
     <div className={css.container}>
       <h1 className={css.header}>Login</h1>
       <Login />
-      <a href="#forgot" className={css.forgot}>
+      <a href="#forgot" className={css.link}>
         Forgot password?
       </a>
+      <p className={css.text}>Or sign in with</p>
       <Socials />
+      <p className={`${css.text} ${css.position}`}>
+        If you dont have acccount yet
+      </p>
+      <a href="#signUp" className={css.link}>
+        Sign up here
+      </a>
     </div>
   );
 }
