b5349c754ab5ec5432ce2b7b28a49914765bae03
BieganskiP <bieganski1996@gmail.com>
2023-02-22 16:15:04 +0100

    s

--- src/components/SignUpWithGoogle.js
+++ src/components/SignUpWithGoogle.js
@@ -1,5 +1,5 @@
 import { auth, googleProvider } from "../config/firebase";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 import googleIcon from "../icons/google.svg";
 import css from "./SignUpWithGoogle.module.css";
 
@@ -12,9 +12,19 @@ export default function SignInWithGoogle() {
     }
   };
 
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
-    <button onClick={signwithGoogle} className={css.button}>
-      <img src={googleIcon} alt="Google Logo"></img>Google
-    </button>
+    <div>
+      <button onClick={signwithGoogle} className={css.button}>
+        <img src={googleIcon} alt="Google Logo"></img>Google
+      </button>
+      <button onClick={logout}>logout</button>
+    </div>
   );
 }
