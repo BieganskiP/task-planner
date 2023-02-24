57c716dd2e95feb1c112a43fbd22f47411677e88
BieganskiP <bieganski1996@gmail.com>
2023-02-24 10:09:30 +0100

    s

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -3,13 +3,13 @@ import LoginScreen from "./LoginScreen";
 import { useState } from "react";
 import css from "./WelcomeScreen.module.css";
 export default function WelcomeScreen() {
-  const [toggle, setToggle] = useState(true);
-  const toggleChecked = () => setToggle((toggle) => !toggle);
+  const [toggleState, setToggle] = useState(true);
+  const toggleChecked = () => setToggle((toggleState) => !toggleState);
 
   return (
     <div className={css.container}>
-      {toggle && <LoginScreen />}
-      {!toggle && <SignUpScreen />}
+      {toggleState && <LoginScreen />}
+      {!toggleState && <SignUpScreen />}
       <button type="button" onClick={toggleChecked} className={css.button}>
         Click here
       </button>
